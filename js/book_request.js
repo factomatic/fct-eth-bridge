49755a54fc1aca8b25d5efdf3ad9324fc32b41ba
Valentin Ganev <sanchopansa@gmail.com>
2019-11-28 03:02:47 +0200

    implement script for booking an existing request

---
+++ js/book_request.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const ethers = require('ethers');
+const toml = require('toml');
+const Bridge = require('../build/Bridge.json');
+
+const args = process.argv.slice(2);
+const config = toml.parse(fs.readFileSync('./config.toml', 'utf-8'));
+const accountPrivKey = config.ethClaimRequest.ethPrivateKey;
+const provider = ethers.getDefaultProvider('homestead');
+const wallet = new ethers.Wallet(accountPrivKey, provider);
+const contract = new ethers.Contract(contractAddress, Bridge.abi, wallet);
+const requestId = args[0];
+
+async function bookRequest(contract, requestId) {
+    let tx = await contract.bookRequest(requestId);
+    let receipt = await tx.wait();
+    return receipt.status;
+}
+
+bookRequest(contract, requestId).then((status) => 
+    if (status == 0) {
+        console.log("Failed to book request.");
+    } else {
+        console.log(`Request ${requestId} successfully booked.`);
+    }
+);
